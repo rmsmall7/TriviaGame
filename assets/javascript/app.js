17da13a7d9d74df4c12c9af7e2f3e72fb234a299
unknown <rmsmall7@gmail.com>
2019-02-27 17:55:16 -0500

    updated js to log correct answer, users' answers and display score

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // Process flow for this javascript
-  //create variables to use.  Totale Questions, score
+  //create variables to use.  Totale Questions, score, correct answers, results
   //create a function to capture the users selection for each question
   //create a prompt that will warn the user when they have missed a quesiton
   //validate the users response to the correct answer
@@ -7,11 +7,11 @@
   // create a display with the correct answers for the users
   //create a timer for the game
 
-  //these are the variables that I created for js
+  //these are the variables that I created for this js
 var totalQuestions = 5;
 var score = 0;
-
-
+var correctAnswers = ["a", "c", "d", "c", "b"]; 
+var results = 0;
 
 //function that will capture the users response for each question.  I used document.getElentByID first since that is what I have been using but could not get it to work.  I found document.forms on https://www.w3schools.com/jsref/coll_doc_forms.asp and this worked.
 //this will store the users' answers in memeory but will not display-that will come later.
@@ -58,5 +58,39 @@ function userAnswers(){
       alert("You didn't answer question 5");
         return false;
      }
+
+     //checking the users answers to the correct answer array.  if the score matches then the score varialbe will increase by 1.  I tested these and they work.  Really want to try and get the for loop to work for this.  These are commented out incase the for loop doesn't work.
+
+    // if(q1 === correctAnswers[0]) {
+      // score++;
+    // }
+    // if(q2 === correctAnswers[1]) {
+     // score++;
+   // }
+   // if(q3 === correctAnswers[2]) {
+    //  score++;
+   // }
+   // if(q4 === correctAnswers[3]) {
+   //   score++;
+   // }
+   // if(q5 === correctAnswers[4]) {
+     // score++;
+   // }
+
+   //for this for loop-  I set the i variable to 1.  I am trying eval again to get the correct question number.  And I have correctAnswers [i-1] so that the correct answer will be pulled from the array.  This is the first time I have tried i-1.  I wanted to test this as I was not sure it would work but it did.  
+  for(i = 1; i<=totalQuestions; i++){
+    if(eval("q" + i) === correctAnswers[i - 1]) {
+      score++;
+    }
+  }
+    //created an alert to make sure I am calculating the score correctly.  This worked so will comment it out.
+    //alert("You scored " + score + " out of " + totalQuestions);
+    
+    //for this will display the score to the user..
+    var results = document.getElementById("results");
+    results.innerHTML = "<h3> You scored <span>" + score + " out of " + totalQuestions + "</span></h3>";
+
+    // I do not want the for to truly submit so I am using the return flase below.   I read about this and used the example from https://javascript.info/forms-submit
+    return false;
     
 }
